ec70256a92907751ff9f3343fd6b713b367d8cf2
Greck2908 <rallistab-82@bk.ru>
2024-05-17 11:28:37 +0500

    add les 5

--- Pyramid_Generator.js
+++ Pyramid_Generator.js
@@ -1,8 +1,14 @@
 /*les1*/
-//let character;
+let character;
 
 /*les2*/
-//let character = "Hello";
+let character = "Hello";
 
 /*les3*/
 let character = 'Hello';
+
+/*les4*/
+let character = 'Hello';
+console.log(character);
+
+/*les5*/
