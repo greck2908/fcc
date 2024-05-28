b0a0f89d8c21961f0140747f1c5e9fc1bcecfa07
Greck2908 <rallistab-82@bk.ru>
2024-05-28 20:41:14 +0500

    add getAverage function

--- Gradebook_App.js
+++ Gradebook_App.js
@@ -1,1 +1,12 @@
 //Building Gradebook App on JS
+// Les 1 Complete getAverage function
+
+function getAverage(scores) {
+    let sum = 0;
+    for (let i = 0; i < scores.length; i++) {
+        sum += scores[i];
+    }
+    return sum / scores.length;
+}
+
+// Les 2
