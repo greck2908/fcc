3cc93cf907496baeaab2699f34b8c902115c197b
Greck2908 <rallistab-82@bk.ru>
2024-05-28 14:42:11 +0500

    add less 101

--- Pyramid_Generator.js
+++ Pyramid_Generator.js
@@ -505,3 +505,17 @@ console.log(numbers);
 //просто удалить все
 
 /*les101*/
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+}
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
+
+/*les102*/
